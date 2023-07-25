c3fc415df0676a7802557c22090bce3646c186b6
Emerson Marini <emerson.marini@getbusy.com>
2023-07-25 09:25:34 +0100

    built static docs

---
+++ storybook-static/assets/Avatar-1443d18f.js
@@ -0,0 +1,24 @@
+import{j as t,a as r,F as f}from"./jsx-runtime-91a467a5.js";import{M as u,b as g,C as o,d as i,A as v,D as e,e as y}from"./index-0bd63522.js";import{A as s,S as A,a as d,I as p,L as h,C as m}from"./Avatar.stories-3c5efe59.js";import{u as c}from"./index-1d576ef5.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-2ed0d667.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-ae8e40e8.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./index-a4cfcc73.js";import"./_baseIsEqual-62e1ad13.js";import"./assert-a1982797.js";import"./index-d37d4223.js";import"./polished.esm-ed3ce7df.js";import"./extends-98964cd2.js";import"./uniq-4dce63e4.js";import"./index-356e4a49.js";import"./emotion-react-jsx-runtime.browser.esm-717d8bd1.js";import"./Icon-35bb54ff.js";import"./index-1fc0ca9a.js";import"./styles-e858c950.js";import"./animation-4340d1f1.js";function l(a){const n=Object.assign({h1:"h1",p:"p",em:"em",h2:"h2",h3:"h3"},c(),a.components);return r(f,{children:[t(n.h1,{id:"avatar",children:"Avatar"}),`
+`,t(u,{of:s}),`
+`,t(g,{children:"Displays an image that represents a user or organization"}),`
+`,t(n.p,{children:"Use an avatar for attributing actions or content to specific users."}),`
+`,r(n.p,{children:["The user's name should ",t(n.em,{children:"always"})," be present when using Avatar – either printed beside the avatar or in a tooltip."]}),`
+`,t(o,{children:t(i,{of:A})}),`
+`,t(v,{of:s}),`
+`,t(n.h2,{id:"usage",children:"Usage"}),`
+`,t(n.p,{children:"Avatar is used to represent a person or an organization. By default the avatar shows an image and gracefully falls back to the first initial of the username. While hydrating the component you may find it useful to render a skeleton template to indicate that Avatar is awaiting data. Avatars can be grouped with the AvatarList component."}),`
+`,t(n.h2,{id:"additional-variants",children:"Additional variants"}),`
+`,t(n.h3,{id:"sizes",children:"Sizes"}),`
+`,t(e,{of:d}),`
+`,t(i,{of:d}),`
+`,t(n.h3,{id:"initials",children:"Initials"}),`
+`,t(e,{of:p}),`
+`,t(i,{of:p}),`
+`,t(n.h3,{id:"loading",children:"Loading"}),`
+`,t(e,{of:h}),`
+`,t(i,{of:h}),`
+`,t(n.h3,{id:"playground",children:"Playground"}),`
+`,t(n.p,{children:"Interact with the component and see how it responds to the different input properties."}),`
+`,t(o,{children:t(i,{of:m})}),`
+`,t(y,{of:m})]})}function H(a={}){const{wrapper:n}=Object.assign({},c(),a.components);return n?t(n,Object.assign({},a,{children:t(l,a)})):l(a)}export{H as default};
+//# sourceMappingURL=Avatar-1443d18f.js.map
