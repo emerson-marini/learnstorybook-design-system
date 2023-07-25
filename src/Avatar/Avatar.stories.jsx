fd8d96a5a9b9c76dbec43f33fcf44acbda5f320a
Emerson Marini <emerson.marini@getbusy.com>
2023-07-25 09:23:18 +0100

    chapter 6

--- src/Avatar/Avatar.stories.jsx
+++ src/Avatar/Avatar.stories.jsx
@@ -3,6 +3,14 @@ import { Avatar } from './Avatar';
 export default {
   title: 'Design System/Avatar',
   component: Avatar,
+  argTypes: {
+    size: {
+      control: {
+        type: 'select',
+      },
+      options: ['tiny', 'small', 'medium', 'large'],
+    },
+  },
 };
 
 export const Standard = {
@@ -13,6 +21,9 @@ export const Standard = {
   },
 };
 
+/**
+ * 4 sizes are supported.
+ */
 export const Sizes = {
   args: {
     username: 'Tom Coleman',
@@ -28,6 +39,9 @@ export const Sizes = {
   ),
 };
 
+/**
+ * Shows the user's initials as a fallback when no image is provided.
+ */
 export const Initials = {
   render: (args) => (
     <>
@@ -39,6 +53,9 @@ export const Initials = {
   ),
 };
 
+/**
+ * Shows a loading indicator.
+ */
 export const Loading = {
   args: {
     loading: true,
@@ -53,6 +70,9 @@ export const Loading = {
   ),
 };
 
+/**
+ * Shows the user's avatar when provided with a `src` prop or in various states and sizes.
+ */
 export const Large = {
   render: () => (
     <>
@@ -67,10 +87,8 @@ export const Large = {
   ),
 };
 
-/*
- * New story using Controls
- * Read more about Storybook templates at:
- * https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+/**
+ * Avatar component using Controls
  */
 export const Controls = {
   args: {
